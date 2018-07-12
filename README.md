8f33f683f9fdb70d169c3df53324f1df2fe4048b
lzenczuk <lucjan.zenczuk@gmail.com>
2018-07-12 12:15:05 +0100

    Add payout endpoint

--- README.md
+++ README.md
@@ -18,6 +18,17 @@ POST http://localhost:8081/accept_offer
 }
 ```
 Response contains json from received from block producer. More useful information can be found in node server console because this is where eosjs logs.
-200 - success
-400 - parameters validation error
-500 - error from EOS server
+- 200 - success
+- 400 - parameters validation error
+- 500 - error from EOS server
+
+POST http://localhost:8081/pay
+```
+{
+	"mb_offer_id": 8778122,
+	"user_win": true
+}
+```
+Field user_win mean is user win in EOS. If user win == true, funds will be transferred to user. If user win == false, funds will be transferred to house.  
+
+- 200 - always 200, even when error. I didn't find out how to handle errors, yet ¯\_(ツ)_/¯ 
--- server.js
+++ server.js
@@ -7,10 +7,44 @@ const bodyParser = require('body-parser');
 const network_address = 'http://jungle.cryptolions.io:38888';
 const betting_contract = 'esbcontrac1';
 const house_account = 'house1';
-//const house_account_private_key = '----REPLACE_WITH_HOUSE_ACCOUNT_PRIVATE_KEY-----';
+const house_account_private_key = '----REPLACE_WITH_HOUSE_ACCOUNT_PRIVATE_KEY-----';
+const betting_contract_private_key = '----REPLACE_WITH_BETTING_ACCOUNT_PRIVATE_KEY-----';
 
 // ----------------------------------
 
+/**
+ * @param mb_offer_id - id of offer in MB
+ * @param winner - 0 user win, 1 - house win
+ */
+function payout(mb_offer_id, winner, res){
+
+    eos = Eos({
+        'httpEndpoint': network_address,
+        'chainId': '038f4b0fc8ff18a4f0842a8f0564611f6e96e8535901dd45e43ac8691a1c4dca',
+        'keyProvider': [ betting_contract_private_key ]
+    });
+
+    eos.transaction(
+        {
+            actions: [
+                {
+                    account: betting_contract,
+                    name: 'payout',
+                    authorization: [{
+                        actor: betting_contract,
+                        permission: 'active'
+                    }],
+                    data: {
+                        "mb_offer_id": mb_offer_id, "winner": winner
+                    }
+                }
+            ]
+        }
+    ).then(function(result){
+        console.log(JSON.stringify(result));
+        res.status(200).send("Check console")
+    })
+}
 
 function accept_offer(offer_id, mb_offer_id, amount_eos, res){
 
@@ -52,6 +86,26 @@ app.post('/accept_offer', function (req, res) {
     }
 
     accept_offer(req.body.offer_id, req.body.mb_offer_id, req.body.amount_eos, res);
+});
+
+app.post('/pay', function (req, res) {
+
+    if(req.body.mb_offer_id===undefined){
+        res.status(400).send('Missing mb_offer_id,');
+        return;
+    }
+
+    if(req.body.user_win===undefined){
+        res.status(400).send('Missing user_win (true/false).');
+        return;
+    }
+
+    let winner = 1;// house win
+    if(req.body.user_win){
+        winner = 0; // user win
+    }
+
+    payout(req.body.mb_offer_id, winner, res)
 });
 
 const server = app.listen(8081, function () {
